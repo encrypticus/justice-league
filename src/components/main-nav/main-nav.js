990f8876c881a40b83dae11d911a5f02ba7898de
encrypticus <encrypticus@gmail.com>
2021-04-16 23:41:54 +0600

    Закончил шапку сайта.

--- src/components/main-nav/main-nav.js
+++ src/components/main-nav/main-nav.js
@@ -1,8 +1,5 @@
-import './main-nav.scss';
 import MetisMenu from 'metismenujs';
 import OnOffCanvas from 'onoffcanvas';
-import 'metismenujs/scss/metismenujs.scss';
-import 'onoffcanvas/scss/onoffcanvas.scss';
 
 const mainNav = new MetisMenu('.main-nav_full .main-nav__list_level-1');
 const mainNavMob = new MetisMenu('.main-nav_mobile .main-nav__list_level-1');
@@ -13,6 +10,7 @@ const mainNavMobCanvas = new OnOffCanvas(mobMenuCanvas);
 
 mainNavMobCanvas.on('show.onoffcanvas', () => {
   hamburger.classList.add('is-active');
+  mobMenuCanvas.classList.add('is-active');
 });
 
 mainNavMobCanvas.on('hide.onoffcanvas', () => {
